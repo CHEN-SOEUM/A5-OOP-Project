c7f30a2fb43aad039a3469cef5433644982293fb
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-04-23 11:03:53 +0700

    create class AirPlane and Seat #5

--- src/airplane/airplane.ts
+++ src/airplane/airplane.ts
@@ -1,4 +1,27 @@
+import { Gate } from "../gate/gate";
+import { Employee } from "../employee/employee";
+import { Seat } from "./seat";
 export class Airplane {
-  // To do
-  // Write the class following the given diagram
+private register_number: string;
+private total_seat: number;
+private seat:Seat[]
+private crews: Employee[];
+private parking_gate: Gate;
+constructor(register_number: string, total_seat: number, seat:Seat[], crew: Employee[], parking_gate: Gate){
+  this.register_number=register_number;
+  this.total_seat=total_seat;
+  this.seat=seat;
+  this.crews=crew;
+  this.parking_gate=parking_gate;
+}
+public getRegisterNumber(): string{
+  return this.register_number;
+}
+public addCrew(Employee):void{}
+
+public setGate(Gate):void{}
+
+public getCrew(): Employee[]{
+  return this.crews;
+}
 }
