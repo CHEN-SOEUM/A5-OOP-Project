32d630d7bd9f8fbd38e63a56f9e0a307be19942e
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-04-25 10:43:05 +0700

    created Trip class #17

--- src/trip/trip.ts
+++ src/trip/trip.ts
@@ -3,12 +3,15 @@ import { Ticket } from "../tickets/ticket";
 export class Trip {
     private trip_id: string;
     private ticket: Ticket[];
-    
+
     constructor(trip_id: string){
         this.trip_id = trip_id;
         this.ticket = [];  
     }
     public getTripId(): string {
         return this.trip_id;
+    }
+    public addTicket(ticket: Ticket): void {
+        this.ticket.push(ticket);
     }
 }
