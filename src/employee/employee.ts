bbd5d5c4d0fb17d4916f49cca01fd10a77482a35
SOEUM.CHEN <SOEUM.CHEN@PNC.LAN>
2024-04-25 11:01:48 +0700

    edit employee class #22

--- src/employee/employee.ts
+++ src/employee/employee.ts
@@ -1,11 +1,44 @@
-import { Person } from "../person/person";
+import { Person, Gender } from "../person/person";
+
 export enum Position {
   pilot = "Pilot",
-  flightAttendant = "Fligt Attendant",
+  flightAttendant = "Flight Attendant",
   manager = "Manager",
   supervisor = "Supervisor",
   staff = "Staff",
 }
+
 export class Employee extends Person {
-  //To do
+  private position: Position;
+  private salary: number;
+
+  constructor(firstName: string, lastName: string, dateOfBirth: string, gender: Gender, position: Position, salary: number) {
+    super(firstName, lastName, dateOfBirth, gender);
+    this.position = position;
+    this.salary = salary;
+  }
+
+  public getSalary(): number {
+    return this.salary;
+  }
+
+  public getPosition(): string {
+    return this.position;
+  }
+
+  getFirstName(): string {
+    return this.firstName;
+  }
+
+  getLastName(): string {
+    return this.lastName;
+  }
+
+  getDateOfBirth(): string {
+    return this.date_of_birth;
+  }
+
+  getGender(): string {
+    return this.gender;
+  }
 }
