3339912dd4bbb1056868a02bf29080d111211b6d
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-04-29 09:15:17 +0700

    add pilot class #23

---
+++ src/pilot/pilot.ts
@@ -0,0 +1,18 @@
+export class Pilot{
+    // private join_flights: Flight[];
+    // constructor(flights: Flight[]){
+    //     this.join_flights = flights;
+    // }
+    public setFlight(Flight):void{
+
+    }
+    // public getFlight():Flight[]{
+    //     return this.join_flights;
+    // }
+    // public getPosition()Position{
+    //     return this.position;
+    // }
+    // public getSalary(): number{
+    //     return this.salary;
+    // }
+}
