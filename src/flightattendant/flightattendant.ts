022a1fe06613553a476219fcc535ed4e9c6f22e1
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-02 08:50:59 +0700

    #31 update diagram

--- src/flightattendant/flightattendant.ts
+++ src/flightattendant/flightattendant.ts
@@ -1,9 +1,12 @@
 import { Flight } from "../flight/flight";
-import { Position } from "../employee/employee";
+import {Employee, Position} from "../employee/employee";
+import {Gender} from "../gender/gender";
+import {Date} from "../date/date";
 
-export class FlightAttendant {
+export class FlightAttendant extends Employee{
     private join_flight: Flight[];
-    constructor() {
+    constructor(name: string,gender: Gender,date_of_birth: Date, position: Position, salary: number) {
+        super(name, gender, date_of_birth,position,salary);
         this.join_flight = [];
     }
     public setFlight(flight:Flight): void{
