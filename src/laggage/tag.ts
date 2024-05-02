8efb7ffcfb93e9acb2ce0083bd73eea0aac514e2
Roth Samreth <140585607+Roth-Samreth@users.noreply.github.com>
2024-05-02 09:13:43 +0700

    #31 update diagram

--- src/laggage/tag.ts
+++ src/laggage/tag.ts
@@ -1,15 +1,15 @@
 import { Flight } from "../flight/flight";
-export type flight={flightNumber:string,code:string}
+export type flight = { flightNumber: string; code: string };
 export class Tag {
   private tag_id: string;
-  private code:string
-  private flight:Object
-  constructor(tag_id: string, code:string) {
+  private code: string;
+  private flight: flight[];
+  constructor(tag_id: string, code: string) {
     this.tag_id = tag_id;
-    this.code=code
+    this.code = code;
   }
-  public setFlight(flight:flight[]):void{
-    this.flight=flight
+  public setFlight(flight: flight[]): void {
+    this.flight = flight;
   }
   public getCode(): string {
     return this.code;
